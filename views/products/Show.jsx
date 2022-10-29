d344c15671a7a0505d409e24b62e74ebdecb168a
KyraCChurch <kchurch0114@gmail.com>
2022-10-29 11:34:19 -0600

    show.jsx

---
+++ views/products/Show.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+
+class Show extends React.Component {
+    render () {
+        const {product, isleNum, inBasket, _id } = this.props.product
+        const prodName = product[0].toUpperCase() + product.substring(1)
+        return( 
+            <Default title={`${capName} Show Page`} fruit={this.props.fruit}>
+                <p>{prodName} is isle {isleNum} and {inBasket ? 'it\'s in the basket' : 'it\'s not in the basket'}</p>
+            </Default>
+        )    
+    }
+}
+
+module.exprots = Show
