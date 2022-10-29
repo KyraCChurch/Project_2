ec2c4e4251ea28f04b21c161d39c8839d77176ec
KyraCChurch <kchurch0114@gmail.com>
2022-10-29 11:49:45 -0600

    Index.jsx

---
+++ views/products/Edit.jsx
@@ -0,0 +1,5 @@
+const React = require('react')
+
+class Edit extends React.Component {
+    
+}
--- views/products/New.jsx
+++ views/products/New.jsx
@@ -5,9 +5,9 @@ class New extends React.Component {
     return (
       <Default title='Add Grocery Item'>
         <form method='POST' action='/products'>
-          Name: <input type='text' name='product' placeholder='Item' /><br />
-          Color: <input type='text' name='color' placeholder='Isle Number' /><br />
-          Is Ready To Eat: <input type='checkbox' name='InBasket' /><br />
+          Product: <input type='text' name='product' placeholder='Item' /><br />
+          Isle Number: <input type='text' name='isleNum' placeholder='Isle Number' /><br />
+          Is In The Basket: <input type='checkbox' name='InBasket' /><br />
           <input type='submit' value='Add Item' />
         </form>
       </Default>
