1ca8cd3b9053626c8d97a7b669d86cc532fc5f08
KyraCChurch <kchurch0114@gmail.com>
2022-10-29 13:36:49 -0600

    edit.jsx

--- views/products/Edit.jsx
+++ views/products/Edit.jsx
@@ -1,5 +1,19 @@
 const React = require('react')
 
 class Edit extends React.Component {
-    
+    render () {
+        const { product, _id, isleNum, inBasket} = this.props.product
+        return (
+            <Default title = {`${product} Edit Page`} product={this.props.product}>
+                <form method='POST' action={`/products/${_id}?_method=PUT`}>
+                Product: <input type='text' name='product' defaultValue={product} /><br />
+                Isle Number: <input type='text' name='isleNum' defaultValue={isleNum} /><br />
+                Is In The Basket: <input type='checkbox' name='InBasket' defaultChecked={inBasket} /> <br />
+                <input type='submit' value='Edit List Item' />
+               </form>
+            </Default>
+        )
+    }
 }
+
+module.exports = Edit
