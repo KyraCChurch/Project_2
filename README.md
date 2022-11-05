9c13bfcae134247d38651d1e1ee3411548aeb770
KyraCChurch <kchurch0114@gmail.com>
2022-11-05 09:10:50 -0600

    readme

--- README.md
+++ README.md
@@ -12,11 +12,13 @@ This spell book is an application where you can log into your own spell book and
 In this application you can signup as a new user and then login. Once logged in you can create spells. After creating a spell you have the option to edit or delete any spell from it's view screen which you reach after clicking it.
 
 ## My Success
-SignUp
+SignUp <br>
 Css
 
 ## Not successful
-Login, is not able to go from login screen to the beginning spell screen.
+Login, is not able to go from login screen to the beginning spell screen. <br>
 Since I cannot reach the beginning spell screen I'm not sure what else was successful vs not. 
 
 ### [Trello Link](https://trello.com/invite/b/RpqG6kJd/ATTIcc3aa57a8bb81dfbd5bb9c45e939a2299CD387FB/project-2)
+
+### [My Project](http://localhost:3000/user/signup)
--- views/spells/Edit.jsx
+++ views/spells/Edit.jsx
@@ -3,13 +3,13 @@ const Default = require('../layouts/Default.jsx')
 
 class Edit extends React.Component {
   render () {
-    const { name, _id, color, readyToEat } = this.props.spell
+    const { name, _id, ingredients, instructions } = this.props.spell
     return (
       <Default title={`${name} Edit Page`} spell={this.props.spell}>
         <form method='POST' action={`/spells/${_id}?_method=PUT`}>
           Name: <input type='text' name='name' defaultValue={name} /><br />
-          Ingredients: <input type='text' name='color' defaultValue={color} /><br />
-          Instructions: <input type='checkbox' name='readyToEat' defaultChecked={readyToEat} /> <br />
+          Ingredients: <input type='text' name='ingredients' defaultValue={ingredients} /><br />
+          Instructions: <input type='text' name='instructions' defaultChecked={instructions} /> <br />
           <input type='submit' value='Edit Spell' />
         </form>
       </Default>
